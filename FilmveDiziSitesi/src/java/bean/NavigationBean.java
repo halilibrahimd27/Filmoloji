d6330e367587c2c55a6911cf3f91fcae557137e8
bberkaysari <bberkaysari0@gmail.com>
2024-05-11 11:54:27 +0300

    navigation2

---
+++ FilmveDiziSitesi/src/java/bean/NavigationBean.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package bean;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ *
+ * @author berkaysari
+ */
+@Named(value = "navigationBean")
+@SessionScoped
+public class NavigationBean implements Serializable {
+
+    /**
+     * Creates a new instance of NavigationBean
+     */
+   public void navigateTo(String page) throws IOException {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.redirect(page + ".xhtml");
+    }
+}
